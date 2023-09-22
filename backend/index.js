97be58a0016f222a52743bd328451175c781b667
SurajJCk <you@example.com>
2023-09-23 02:39:55 +0530

    bookModel

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,31 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("This is a Bookstore MERN App");
+});
+
+//Route for Save a Book
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
