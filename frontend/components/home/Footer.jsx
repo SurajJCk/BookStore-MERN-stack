8fb8dac72898d41814130baa0b236e807d0c77da
SurajJCk <you@example.com>
2023-09-28 17:32:59 +0530

    update

---
+++ frontend/components/home/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-4 text-center text-gray-600">
+      &copy; {new Date().getFullYear()} Suraj Jyoti Changkakoti. All Rights
+      Reserved.
+    </div>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -26,6 +26,14 @@ const Home = () => {
 
   return (
     <div className="p-4">
+      <div className="flex justify-center">
+        <button
+          className="bg-teal-300 px-4 py-1 rounded-lg mb-4"
+          onClick={() => setShowType("table")}
+        >
+          Bookstore
+        </button>
+      </div>
       <div className="flex justify-center items-center gap-x-4">
         <button
           className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
